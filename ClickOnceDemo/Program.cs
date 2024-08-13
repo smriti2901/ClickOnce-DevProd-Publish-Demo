9f48121fc0923937c34e85d1dfd870599105b10a
Smriti Gupta <smriti2901@gmail.com>
2024-08-13 15:14:48 +0200

    ClickOnce demo app

---
+++ ClickOnceDemo/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+#if DEV
+Console.WriteLine("Hello, World! (Dev)");
+#else
+Console.WriteLine("Hello, World! (Prod)");
+#endif
+
+Console.WriteLine("Press any key to exit.");
+Console.ReadKey();
